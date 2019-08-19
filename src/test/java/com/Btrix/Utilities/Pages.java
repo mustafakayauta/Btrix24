3a5c4f22d7e65ebe69cf48ab3aaaf7fdb9eb2bf1
mustafakayauta <mustafakayauta@gmail.com>
2019-08-19 14:52:49 -0500

    first commit

---
+++ src/test/java/com/Btrix/Utilities/Pages.java
@@ -0,0 +1,23 @@
+package com.Btrix.Utilities;
+
+
+
+public class Pages {
+
+    /*private LoginPage loginPage;
+    private TaskPage taskPage;
+
+    public LoginPage loginPage() {
+        if (loginPage == null) {
+            loginPage = new LoginPage();
+        }
+        return loginPage;
+    }
+
+    public TaskPage taskPage(){
+        if (taskPage == null) {
+            taskPage = new TaskPage();
+        }
+        return taskPage;
+    }*/
+}
